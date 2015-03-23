071dd18ac11f2105363524c61234bb90b69e4c88
Grayson Carroll <carrollgt91@gmail.com>
2015-03-22 19:23:35 -0500

    Patches documentation on sqlgen.clj

--- src/malachite_migrations/sqlgen.clj
+++ src/malachite_migrations/sqlgen.clj
@@ -1,9 +1,20 @@
 (ns malachite-migrations.sqlgen)
 
 (defprotocol SQLGenerator
-  (create-table [this table-name columns])
-  (drop-table [this table-name])
-  (add-column [this table-name [col-name col-type]])
-  (remove-column [this table-name column-name])
-  (add-index [this table-name columns index-config] [this table-name columns])
-  (remove-index [this table-name columns index-name] [this table-name columns]))
+  (create-table 
+    [this table-name columns]
+    "Generates SQL to create a table.")
+  (drop-table 
+    [this table-name]
+    "Generates SQL to drop a table.")
+  (add-column 
+    [this table-name [col-name col-type]]
+    "Generates SQL to add a column to a table.")
+  (remove-column 
+    [this table-name column-name]
+    "Genertes SQL to remove a column from a table.")
+  (add-index
+    [this table-name columns index-config] [this table-name columns]
+    "Generates SQL to add an index to one or more columns on a table.")
+  (remove-index  [this table-name columns index-name] [this table-name columns]
+    "Generates SQL to remove an index from one or more columns on a table."))
